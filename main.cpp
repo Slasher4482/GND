3b70182b0d511a2419d78f62b4c29c10bc97cc83
iRewiewer <irewiewer@gmail.com>
2019-10-05 22:05:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,57 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+int arr[9999];
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Apple");
+    int limit, number;
 
-    // run the program as long as the window is open
-    while (window.isOpen())
+    cout << "How many numbers do you have?" << endl;
+    cout << "Number: ";
+    cin >> limit;
+
+    cout << endl;
+
+    cout << "Which numbers?" << endl;
+    cout << "Array: ";
+
+    for(int i = 1 ; i <= limit ; i++)
+        cin >> arr[i];
+
+    cout << endl;
+
+    cout << "Which number's do you want to find?" << endl;
+    cout << "Target: ";
+    cin >> number;
+
+    cout << endl;
+
+    int result;
+    int left = 1;
+    int right = limit;
+
+    int mid;
+
+    while (left <= right)
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
+        mid = (left + right) / 2;
+
+        if(number == arr[mid])
         {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
+            result = mid;
+            left = right + 1;
         }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        window.draw(sprite);
-
-        // end the current frame
-        window.display();
+        else
+            if (number > arr[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
     }
 
-    return 0;
+    if(result == mid)
+        cout << "The number " << arr[result] << " was found at the element with index " << result << endl;
+    else
+        cout << "The number " << number << " was not found." << endl;
 }
